2a5237b4515fa0e2cbaf8b16f5ae1050154321f4
R. LaRoi <are@raewhoo.com>
2024-04-17 23:03:42 -0400

    File Init

---
+++ src/app/LoadPage.tsx
@@ -0,0 +1,18 @@
+"use client"
+import React from "react";
+import './Stylesheets/loading.css'
+
+export const LoadPage = (): React.ReactElement => {
+
+  return (
+ <div className="load-body h-screen">
+<span className="loader "></span>
+
+  </div>   
+//   
+//   <div className="one"></div>
+//   <div className="two"></div>
+
+  );
+ 
+}
