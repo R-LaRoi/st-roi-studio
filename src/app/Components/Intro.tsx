07561cdc28e2bae2a64b50620ac172f7fae14f26
R. LaRoi <are@raewhoo.com>
2024-04-19 19:09:32 -0400

    About Section
    Cards
    Grid

---
+++ src/app/Components/Intro.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Stylesheets/intro.css'
+
+export const Intro = (): React.ReactElement => {
+return(
+<section>
+</section>
+
+
+
+
+);}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import {LoadPage} from './Components/LoadPage'
 import {NavBar} from './Components/Navigation'
-
+import { Intro } from "./Components/Intro";
 
 
 export default function Home() {
@@ -30,25 +30,87 @@ export default function Home() {
 </main>
 </section>
 
-<section className="intro-container ">
-  <div className="cards">
-    <div className="card-one">
-      Hi! I'm Rachel Stroy. I work with people to create web experiences that are user friendly and designed to go places. 
-    </div>
- <div className="">
-  <video playsInline autoPlay muted loop className='card-two' >
-        <source
-          src="https://github-production-user-asset-6210df.s3.amazonaws.com/114012059/324076058-6db53246-f840-47c1-981d-590307ba2fd4.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240419%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240419T183427Z&X-Amz-Expires=300&X-Amz-Signature=3569fd2904e6d86036241533e3be4aa4ab3da7a6ff2e3c31accefa051acfefa1&X-Amz-SignedHeaders=host&actor_id=114012059&key_id=0&repo_id=788236026"
-          type="video/mp4"
-        />
-      </video>
+<section className="--intro ">
+<div className="col-1">  Hi! I'm Rachel Stroy. I work with people to create web experiences that are user friendly and designed to go places. </div>
 
 
-     {/* <img  className="card-two" src="https://github.com/R-LaRoi/st-roi-studio/assets/114012059/ad4bc82d-3351-4f17-ae2a-66824e7bf460" alt="woman jumping rope"  /> */}
-    </div>
-  </div>
-
+<img  className="col-2" src="https://github.com/R-LaRoi/st-roi-studio/assets/114012059/ad4bc82d-3351-4f17-ae2a-66824e7bf460" alt="woman jumping rope"  />
 </section>
+
+<div className="horizontal-line"
+></div>
+<section className="about ">
+  <div className="abt-title">
+    about me
+  </div>
+<div>
+As a creative developer, I focus on both design and development — allowing me to work on projects from concept until deployed. Focusing on unique layouts, interactions and great typography to offer user friendly web experiences.
+</div>
+</section>
+<div className="horizontal-line"
+></div>
+<section className="about ">
+  <div className="abt-title">
+ Here's what I do
+  </div>
+<div className="--cards">
+<div>
+<div className="flex px-3 py-3 bg-neutral-700">
+    <div className="max-w-sm rounded overflow-hidden ">
+             <svg
+      viewBox="0 0 1024 1024"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+ 
+    >
+      <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM513.1 518.1l-192 161c-5.2 4.4-13.1.7-13.1-6.1v-62.7c0-2.3 1.1-4.6 2.9-6.1L420.7 512l-109.8-92.2a7.63 7.63 0 01-2.9-6.1V351c0-6.8 7.9-10.5 13.1-6.1l192 160.9c3.9 3.2 3.9 9.1 0 12.3zM716 673c0 4.4-3.4 8-7.5 8h-185c-4.1 0-7.5-3.6-7.5-8v-48c0-4.4 3.4-8 7.5-8h185c4.1 0 7.5 3.6 7.5 8v48z" />
+    </svg>
+        <div className="  py-2">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-white text-base pt-16">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                perferendis eaque, exercitationem praesentium nihil.
+            </p>
+        </div>
+        <div className=" py-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+         
+        </div>
+    </div>
+</div>
+</div>
+<div>
+<div className="flex px-3 py-3 bg-neutral-700 ">
+    <div className="max-w-sm rounded overflow-hidden ">
+       <svg
+      viewBox="0 0 1024 1024"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+ 
+    >
+      <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM513.1 518.1l-192 161c-5.2 4.4-13.1.7-13.1-6.1v-62.7c0-2.3 1.1-4.6 2.9-6.1L420.7 512l-109.8-92.2a7.63 7.63 0 01-2.9-6.1V351c0-6.8 7.9-10.5 13.1-6.1l192 160.9c3.9 3.2 3.9 9.1 0 12.3zM716 673c0 4.4-3.4 8-7.5 8h-185c-4.1 0-7.5-3.6-7.5-8v-48c0-4.4 3.4-8 7.5-8h185c4.1 0 7.5 3.6 7.5 8v48z" />
+    </svg>
+        <div className="  py-2">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-white text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                perferendis eaque, exercitationem praesentium nihil.
+            </p>
+        </div>
+        <div className=" py-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+         
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</section>
+
 </>
 ) 
 
